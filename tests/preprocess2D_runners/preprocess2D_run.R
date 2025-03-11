4f0b01290c3a9ec1b45119eed7a6f159221ab120
danieeeld2 <danieeeld2@gmail.com>
2025-03-11 10:18:29 +0100

    Created R runner for preprocess2D closes #11
    This code allows to recive a dataset in CSV format and generate the outputs of preprocess2D R version into CSV format

---
+++ tests/preprocess2D_runners/preprocess2D_run.R
@@ -0,0 +1,17 @@
+args <- commandArgs(trailingOnly=TRUE)
+# Take first argument (It should be the dataset)
+dataset <- args[1]  
+
+# Load the preprocess2D function
+source("../../R/preprocess2D.R")   
+
+# Read data from the CSV file
+X <- as.matrix(read.csv(dataset, header=FALSE)) 
+
+# Run the preprocess2D function
+result <- preprocess2D(X)
+
+# Save the results to CSV files
+write.csv(result$xcs, "preprocess2D_r.csv", row.names=FALSE)
+write.csv(result$average, "average_r.csv", row.names=FALSE)
+write.csv(result$scale, "scale_r.csv", row.names=FALSE)
