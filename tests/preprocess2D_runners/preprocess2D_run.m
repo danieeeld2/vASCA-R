b29983f4b5d829ab369b3714fe8c890497492459
danieeeld2 <danieeeld2@gmail.com>
2025-03-11 12:06:35 +0100

    :bug: fixed preprocess2D_run.m closes #15
    We have changed the path to the MATLAB script and correct the logic for receive the dataset as an argument

--- tests/preprocess2D_runners/preprocess2D_run.m
+++ tests/preprocess2D_runners/preprocess2D_run.m
@@ -1,7 +1,22 @@
-function run_preprocess2D(dataset)
+function run_preprocess2D()
+    % Get arguments
+    args = argv();
+
+    % Check number of arguments (We need one dataset)
+    if numel(args) < 1
+        error('Not enough arguments. Usage: preprocess2D_run <dataset>');
+    end
+
+    dataset = args{1};  % Get dataset
+
+    % Check that it is a CSV file
+    if ~strcmp(dataset(end-3:end), '.csv')
+        error('Dataset must be a CSV file');
+    end
+
     % Add path to preprocess2D.m script
-    
-    aaddpath('../../matlab');
+    addpath('../matlab');
+
     % Read dataset from CSV file
     X = csvread(dataset);
 
@@ -13,3 +28,5 @@ function run_preprocess2D(dataset)
     csvwrite('average_matlab.csv', av);
     csvwrite('scale_matlab.csv', sc);
 end
+
+run_preprocess2D();
